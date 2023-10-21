184e9d83a162d3907d4e4c5ba3890139798f0cf0
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2023-10-22 02:19:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 24082128
+    API_HASH = "dfac987a12a41d1075e691d94f9d4802"
+    TOKEN = "5811177842:AAFu2an4eA3isAw4I3c1aQjZTWrxccElEDE"  
+    START_PIC = "https://telegra.ph/file/d8bb48e8f3ab4acaf3775.jpg" 
+    CHAT = "BotzClub"    
